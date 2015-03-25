509ab60e38e5b6468a0fb4fe32db0e55d9c1c0d3
pingyen <pingyen@yahoo-inc.com>
2015-03-25 08:59:59 +0800

    Added tool index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8" >
+<title>Vexed's Online Tools</title>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css" rel="stylesheet" >
+<style>
+    body {
+        font-family: Lora, serif;
+    }
+
+    body > h1 {
+        border-bottom: 1px solid #EBEBEB;
+        font-size: 32px;
+        margin: 0 12px;
+        padding: 18px 0 12px;
+    }
+
+    body > ul {
+        margin: 12px 0;
+    }
+
+    body > ul > li {
+        line-height: 2;
+        font-size: 18px;
+    }
+
+    body > ul > li > a {
+        color: #000;
+        text-decoration: none;
+    }
+
+    body > ul > li > a:hover {
+        text-decoration: underline;
+    }
+</style>
+</head>
+<body>
+<h1>Vexed's Online Tools</h1>
+<ul>
+    <li><a href="html2image" >Convert HTML to Image</a></li>
+    <li><a href="life900months" >Life is only 900 months</a></li>
+</ul>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-6851063-2', 'auto');
+    ga('send', 'pageview');
+</script>
+</body>
+</html>
